3440c46e6ae32071a7a9b5c532dcc18326e9b8cc
FACON-Nicolas <faconicolas@gmail.com>
2023-05-13 10:03:26 +0100

    update profile

--- resources/views/my.blade.php
+++ resources/views/my.blade.php
@@ -5,28 +5,27 @@
         .pp {
             background-image: url({{ asset($user->picturePath) }});
             background-size: cover;
-            width: 15vw;
-            height: 15vw;
         }
     </style>
-    <div class="container">
+    <div>
         <div>
             <div>
-                <div class="mt-10 mb-10">
-                    <div class="flex flex-row gap-5 px-5">
-                        <div class="rounded-full pp"></div>
-                        <h4 class="text-2xl mb-2">{{$user->name}}</h4>
+                <div class="py-10 mx-auto">
+                    <div class="flex flex-row gap-5 px-5 mx-auto lg:w-1/2 md:w-3/5 w-5/6">
+                        <div class="rounded-full pp lg:w-40 lg:h-40 md:w-32 md:h-32 w-20 h-20"></div>
+                        <div>
+                            <div class="ml-10 flex flex-row gap-5 mx-auto">
+                                <h4 class="text-2xl mt-2">{{$user->name}}</h4>
+                                <div class="bg-white text-black h-10 px-4 flex items-center justify-center rounded-xl">
+                                    <a class="hover:text-green-400" href="{{route("profile.edit")}}"><b>Edit Profile</b></a>
+                                </div>
+                            </div>
+                            <div class="mt-5 flex flex-row ml-10 gap-10">
+                                <x-profile.followed :followed="$user->followed"></x-profile.followed>
+                            </div>
+                        </div>
                     </div>
-                     <a class="hover:text-green-400"
-                                                                      href="{{route("profile.edit")}}">Edit</a>
-                    <h5 class="text-1xl mb-2">Member since {{date_format($user->created_at,"j F Y")}}</h5>
-                    <div class="text-2xl">{{$user->email}}</div>
                 </div>
-            </div>
-            <div>
-                <x-profile.follower :follower="$user->follower"></x-profile.follower>
-                <x-profile.followed :followed="$user->followed"></x-profile.followed>
-                <x-profile.posts :posts="$posts"></x-profile.posts>
             </div>
         </div>
     </div>

17d59dcf5582c66ed10011c5b3fe4697cc7d1629
FACON-Nicolas <faconicolas@gmail.com>
2023-05-13 20:35:44 +0100

    change redirect

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -127,7 +127,7 @@ class PostsController extends Controller
         $post = Post::where('slug', $slug);
         $post->delete();
 
-        return redirect('/');
+        return redirect()->back();
     }
 
     public function publish($slug)

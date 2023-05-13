41e52d10209173f8e99e90852149bea13628533d
FACON-Nicolas <faconicolas@gmail.com>
2023-05-13 17:23:14 +0100

    add logout and delete

--- resources/views/my.blade.php
+++ resources/views/my.blade.php
@@ -7,6 +7,17 @@
             background-size: cover;
         }
     </style>
+    <div class="absolute right-10 top-24 lg:flex-row gap-2 hidden lg:flex ">
+        <form method="post" action="{{route('logout')}}">
+            @csrf
+            <button class="drop-shadow-md bg-red-500 p-3 hover:bg-red-400 rounded" type="submit">Logout</button>
+        </form>
+        <form method="post" action="{{route('profile.delete')}}">
+            @method('DELETE')
+            @csrf
+            <button class="drop-shadow-md bg-red-700 p-3 hover:bg-red-600 rounded" type="submit">Delete profile</button>
+        </form>
+    </div>
     <div>
         <div>
    <!-- <div class="container mx-auto text-center">
@@ -29,16 +40,29 @@
             </div>-->
             <div>
                 <div class="py-10 mx-auto">
-                    <div class="flex flex-row gap-5 px-5 mx-auto lg:w-1/2 md:w-3/5 w-5/6">
+                    <div class="flex flex-row gap-5 mx-auto lg:w-1/2 md:w-3/5 w-5/6">
                         <div class="rounded-full pp lg:w-40 lg:h-40 md:w-32 md:h-32 w-20 h-20"></div>
                         <div>
-                            <div class="ml-10 flex flex-row gap-5 mx-auto">
+                            <div class="ml-5 flex flex-row gap-5 mx-auto">
                                 <h4 class="text-2xl mt-2">{{$user->name}}</h4>
-                                <div class="bg-white text-black h-10 px-4 flex items-center justify-center rounded-xl">
-                                    <a class="hover:text-green-400" href="{{route("profile.edit")}}"><b>Edit Profile</b></a>
-                                </div>
+                                @if(Auth::user()->id === $user->id)
+                                    <div class="bg-white text-black hover:text-white hover:bg-black h-10 px-4 flex items-center justify-center rounded-xl">
+                                        <a href="{{route("profile.edit")}}"><b>Edit</b></a>
+                                    </div>
+                                    <div class="flex-row gap-2 lg:hidden flex">
+                                        <form method="post" action="{{route('logout')}}">
+                                            @csrf
+                                            <button class="drop-shadow-md bg-red-500 p-3 hover:bg-red-400 rounded" type="submit">Logout</button>
+                                        </form>
+                                        <form method="post" action="{{route('profile.delete')}}">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button class="drop-shadow-md bg-red-700 p-3 hover:bg-red-600 rounded" type="submit">Delete</button>
+                                        </form>
+                                    </div>
+                                @endif
                             </div>
-                            <div class="mt-5 flex flex-row ml-10 gap-10">
+                            <div class="mt-5 flex flex-row ml-5 gap-10">
                                 <x-profile.follower :follower="$user->follower"></x-profile.follower>
                                 <x-profile.followed :followed="$user->followed"></x-profile.followed>
                                 <x-profile.posts :posts="$posts"></x-profile.posts>

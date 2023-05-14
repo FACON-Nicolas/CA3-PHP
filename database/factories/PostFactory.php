ef0a8fd1ad8d59ae4b6c742d2cb1724a6e118770
JOAO-Ethan <joao.ethan@orange.fr>
2023-05-14 22:09:44 +0100

    Diversify posts' pictures

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -13,13 +13,21 @@ class PostFactory extends Factory
      */
 
     public $count = 0;
+
     public function definition()
     {
         return [
-            'slug' => 'slug'.$this->count,
-            'title' => 'title'.$this->count,
-            'description' => 'description'.$this->count++,
-            'image_path' => 'profiles/defaultPicture.png',
+            'slug' => 'slug' . $this->count,
+            'title' => 'title' . $this->count,
+            'description' => 'description' . $this->count++,
+            'image_path' => $this->faker->randomElement([
+                '644bc54fd6861-Hello.jpg',
+                '644bcdb767611-Hello.jpg',
+                '6034f2b4ac8f1-This is my title.jpg',
+                '64463ba655fe1-Hello !!.png',
+                '64464d8124a88-Hello !.png',
+                '64465018eda73-Test.jpg',
+                ]),
             'user_id' => $this->faker->numberBetween(1, 10),
             'draft' => false
 

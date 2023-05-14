36771d3085b1a1141dc5ed262b5a143df911694c
FACON-Nicolas <faconicolas@gmail.com>
2023-05-14 18:38:00 +0100

    add something when ou follow anyone

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,10 @@
             width: 35rem;
         }
 
+        .full {
+            height: 85vh;
+        }
+
         @media only screen and (max-width: 700px) {
             .main-index {
                 width: 100vw;
@@ -16,5 +20,9 @@
     <main class="main-index mx-auto">
         <x-liste-post :posts="$posts"></x-liste-post>
     </main>
-
+    @if ($posts->count() == 0)
+        <div class="flex items-center justify-center full font-bold text-2xl">
+            <p>you follow anyone...<br/>Please follow someone to see posts (check explorer to see more posts)</p>
+        </div>
+    @endif
 @endsection

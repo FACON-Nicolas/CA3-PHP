64cccf6a72fe2e9a88cc229f735123ad1585f6e3
JOAO-Ethan <joao.ethan@orange.fr>
2023-05-14 22:26:35 +0100

    Display of the post's image

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -13,7 +13,7 @@
     <span class="text-gray-500">
         By <a href="{{route("profile", $post->user->id)}}" class="font-bold italic text-gray-800">{{ $post->user->name }}</a>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
     </span>
-
+    <img class="w-full post-bg" src="{{ asset('images/'.$post->image_path) }}" alt="">
     <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
         {{ $post->description }}
     </p>
